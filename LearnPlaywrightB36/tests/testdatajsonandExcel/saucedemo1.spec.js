cbed7a453f4fff17c7c601c5d07a6a9e40f60b54
vittalap-collab <vittalap12@gmail.com>
2026-05-19 16:19:49 +0530

    saucedemo

--- LearnPlaywrightB36/tests/testdatajsonandExcel/saucedemo1.spec.js
+++ LearnPlaywrightB36/tests/testdatajsonandExcel/saucedemo1.spec.js
@@ -4,9 +4,9 @@ import data from "../../testdata/addemploye.json"
 test('verify login as standard user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill(data.firstname);
+  await page.locator('[data-test="username"]').fill(data.Username);
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill(data.lastname);
+  await page.locator('[data-test="password"]').fill(data.Password);
   await page.locator('[data-test="login-button"]').click();
   await page.locator('[data-test="title"]').click();
   await expect(page.locator('[data-test="title"]')).toBeVisible();
