2c082841a92c1a7c56735691625cb1eb50d9b5ed
vittalap-collab <vittalap12@gmail.com>
2026-05-21 08:28:12 +0530

    buzzpost

---
+++ LearnPlaywrightB36/tests/Buzz/Postmmessege.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import Data from '../../testdata/Buzzpost.json';
+
+test('verify post message in buzz', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.locator('//input[@name="username"]').fill("Admin");
+    await page.locator('//input[@name="password"]').fill("admin123");
+    await page.locator('//button[@type="submit"]').click();
+
+    await page.locator('//a[@href="/web/index.php/buzz/viewBuzz"]').click();
+    await page.locator(`//textarea[@placeholder="What's on your mind?"]`).fill(Data.text);
+    await page.locator('//button[@type="submit"]').click();
+    //await expect(page.locator('//p[@class="oxd-grid-item oxd-grid-item--gutters"]')).toHaveText(Data.text);
+
+
+});
